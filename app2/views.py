c40c996707b24fe3aca50f4bb6ab05f30d475864
pillagovula hemavathi <pillagovulahemavathi86>
2022-12-29 17:55:10 +0530

    first commit

---
+++ app2/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def first(request):
+    return HttpResponse('<h1>this is app2 first view </h1>')
+
+
+def second(request):
+    return HttpResponse('<h1> this is app2 second view</h1>')
